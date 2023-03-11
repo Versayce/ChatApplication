8a36ddfe8b0a87e2dbfc740d58c90e79dab23d13
Alex <acastro166@gmail.com>
2023-03-10 23:36:01 -0700

    added conditional rendering logic for pm chat names based on who is logged in

--- react-app/src/components/dithcord/Channels/Channels.js
+++ react-app/src/components/dithcord/Channels/Channels.js
@@ -52,8 +52,10 @@ function Channels({ channels }) {
                 {currentChats && currentChats.map((chat) => (
                     <ChannelOptions key={chat.id} >
                         <Chat onClick={() => getOneChat(chat.id)} key={chat.id}>
-                            {/* {console.log('', '\n', '--------------CHAts COMPONENT DATA--------------', '\n', channel, '\n', '')} */}
-                            <h3 key={chat.id}>{chat.name}</h3>
+                            {console.log('', '\n', '--------------CHAts COMPONENT DATA--------------', '\n', chat, '\n', '')}
+                            <h3 key={chat.id}>
+                                {sessionUser.username === chat.users[0].username ? chat.users[1].username : chat.users[0].username}
+                            </h3>
                         </Chat>
                         
                         <ChannelOptionButtons>
